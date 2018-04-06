b68e980834a2eda9926b3b1639c33dc0203e88e5
Sofia <16619330+SofiaMcLeod314@users.noreply.github.com>
2018-04-06 20:11:42 +0930

    Add files via upload

--- milestone-2-building/placeholder.js
+++ milestone-2-building/placeholder.js
@@ -7,6 +7,15 @@ var date_departure;
 var total_price;
 var price;
 var number_nights;
+var Placename;
+var Locations;
+var Hotelprice;
+// storing last page before signup/login page
+var page_before_login_signup;
+// if login/signup button was pressed
+var l_s;
+//if submitted login/signup Details
+var submit_l_s;
 
 //  PLACES IN THIS AREA
 function book_details(hotel, location, night_price){
@@ -23,8 +32,17 @@ function submit_details(){
   number_children = $("#child_number option:selected").text();
   date_arrival = $("#from_date").val();
   date_departure = $("#to_date").val();
+
+  // ERROR HERE - WONT SEND DETAILS ????????
+
   $("#booking_your_hotel_div").hide();
-  $("#lets_book_this_div").show();
+  if (submit_l_s==1){
+    write_confirmation();
+    $("#confirmation_div").show();
+  }
+  else{
+    $("#lets_book_this_div").show();
+  }
 }
 function signup(){
   $("#lets_book_this_div").hide();
@@ -35,14 +53,30 @@ function login(){
   $("#login_div").show();
 }
 function submit_signup(){
-  $("#signup_div").hide();
-  $("#confirmation_div").show();
-  write_confirmation();
+  if (l_s==1){
+    $("#signup_div").hide();
+    $("#"+page_before_login_signup).show();
+  }
+  else{
+    $("#signup_div").hide();
+    $("#confirmation_div").show();
+    write_confirmation();
+  }
+  submit_l_s = 1;
+
 }
 function submit_login(){
-  $("#login_div").hide();
-  $("#confirmation_div").show();
-  write_confirmation();
+  if (l_s==1){
+    $("#login_div").hide();
+    $("#"+page_before_login_signup).show();
+  }
+  else{
+    $("#login_div").hide();
+    $("#confirmation_div").show();
+    write_confirmation();
+  }
+  submit_l_s = 1;
+
 }
 
 function write_confirmation() {
@@ -59,6 +93,9 @@ function write_confirmation() {
 	$("#dep_date").html(date_departure);
 	$("#price_total").html("$"+total_price);
 }
+
+
+
 
 function assign_number_nights() {
 	var t1 = toDate(date_arrival),
@@ -80,6 +117,11 @@ function days_between(t1, t2) {
 	return (d2-d1);
 }
 
+
+
+
+
+
 function back_button_hotels(){
   $("#places_and_area_div").hide();
   $("#search_div").show();
@@ -93,14 +135,81 @@ function back_button_login_signup(){
   $("#booking_your_hotel_div").show();
 }
 function back_button_signup(){
+  if (l_s==1){
+    $("#"+page_before_login_signup).show();
+  }
+  else{
+    $("#lets_book_this_div").show();
+  }
   $("#signup_div").hide();
-  $("#lets_book_this_div").show();
 }
 function back_button_login(){
+  if (l_s==1){
+    $("#"+page_before_login_signup).show();
+  }
+  else{
+    $("#lets_book_this_div").show();
+  }
   $("#login_div").hide();
-  $("#lets_book_this_div").show();
 }
 function back_button_confirmation(){
   $("#confirmation_div").hide();
-  $("#lets_book_this_div").show();
+  if (submit_l_s==1){
+    $("#booking_your_hotel_div").show();
+  }
+  else{
+    $("#lets_book_this_div").show();
+  }
+}
+
+function back_button_hotel_details(){
+  $("#hotel_details_div").hide();
+  $("#places_and_area_div").show();
+}
+
+function go_home(){
+  //$("div").hide();
+  //$("#search_div").show();
+}
+function signup_button(){
+  //stores the id of the previous page
+  page_before_login_signup = $("div.feature_page").not(":hidden").prop("id");
+  $("div.feature_page").hide();
+  $("#signup_div").show();
+  l_s = 1;
+
+}
+function login_button(){
+  //stores the id of the previous page
+  page_before_login_signup = $("div.feature_page").not(":hidden").prop("id");
+  $("div.feature_page").hide();
+  $("#login_div").show();
+  l_s = 1;
+}
+
+
+// for details page
+var misc_attr = ["wifi", "pool", "spa", "undercover parking", "restaurant", "balcony", "etc"];
+function write_misc_info() {
+  document.querySelector("#misc_info").innerHTML =
+  [misc_attr[0], misc_attr[2], misc_attr[4]].join(" : ");
+}
+
+
+function details(placename,location,price,img){
+  Placename=placename;
+  Locations=location;
+  Hotelprice=price;
+  $("#places_and_area_div").hide();
+  $("#hotel_details_div").show();
+  $("#image_details").attr("src",img);
+  $("#name_details").html(placename);
+  $("#price_details").html("$"+price);
+  $("#location_details").html(location);
+
+
+}
+function book_now_button_detials(){
+  book_details(Placename,Locations,Hotelprice);
+  $("#hotel_details_div").hide();
 }
